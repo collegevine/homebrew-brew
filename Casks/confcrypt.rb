7c21a899af413560cebd0bb4910726cd943947e0
Chris Coffey <cmcof273@gmail.com>
2019-01-19 22:39:25 -0500

    Add ConfCrypt cask

---
+++ Casks/confcrypt.rb
@@ -0,0 +1,10 @@
+cask 'confcrypt' do
+  version 'v0.2.0'
+  sha256 'ef635317085dbdaad021c740846f41015fd1b2ff8084e75e16124209e3e6a6a2'
+
+  url "https://github.com/collegevine/confcrypt/releases/download/#{version}/confcrypt.tar.gz"
+  name 'ConfCrypt'
+  homepage 'https://github.com/collegevine/confcrypt'
+
+  binary 'confcrypt'
+end
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # homebrew-brew
 Collegevine's hosted taps and casks
+
+To add these to your local `brew` instalation, run `brew tap collegevine/brew`. You'll then be able to access our Formula or Casks.
