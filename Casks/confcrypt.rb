47f16960b2082cef8886fac7f420274f747f4bd8
Ralph Morton <ralphmorton@gmail.com>
2019-02-04 18:28:45 +0200

    Update confcrypt binary URL

--- Casks/confcrypt.rb
+++ Casks/confcrypt.rb
@@ -2,7 +2,7 @@ cask 'confcrypt' do
   version 'v0.2.0'
   sha256 'ef635317085dbdaad021c740846f41015fd1b2ff8084e75e16124209e3e6a6a2'
 
-  url "https://github.com/collegevine/confcrypt/releases/download/#{version}/confcrypt.tar.gz"
+  url "https://github.com/collegevine/confcrypt/releases/download/#{version}/confcrypt-darwin-x64.tar.gz"
   name 'ConfCrypt'
   homepage 'https://github.com/collegevine/confcrypt'
 
