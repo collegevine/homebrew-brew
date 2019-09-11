67713ae5813278955f88969dd1c96472b59895e5
Chris Coffey <cmcof273@gmail.com>
2019-09-11 10:42:17 -0400

    Update latest to 0.2.4

--- Casks/confcrypt.rb
+++ Casks/confcrypt.rb
@@ -1,6 +1,6 @@
 cask 'confcrypt' do
-  version 'v0.2.3'
-  sha256 '9a5939c4268ba7f43753e3b6e03198a304296f6bcc31ef9fdafd4bbd04f2cb89'
+  version 'v0.2.4'
+  sha256 '3705e6eebc05ce6f33c5fa72911b1a31d5f2c9abfb071c5ce9d096d332d3055c'
 
   url "https://github.com/collegevine/confcrypt/releases/download/#{version}/confcrypt-darwin-x64.tar.gz"
   name 'ConfCrypt'
